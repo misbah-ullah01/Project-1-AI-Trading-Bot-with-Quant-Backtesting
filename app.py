66a8f24b0040352a6f5d8f146ca2805765eb056d
Misbah Ullah <misbahu094@gmail.com>
2026-03-27 22:42:39 +0500

    Added Ticker and last row details

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print ("git upload successful test 3")
+import streamlit as st
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("Project 1 - AI Trading Bot with Quant Backtesting")
+st.subheader("Day 1: Live Market data fetcher")
+
+#Fetch data
+ticker = st.selectbox("Choose Crypto to Fetch:", ["BTC-USD", "XRP-USD"]);
+data = yf.download(ticker, period="30d")
+
+st.write("Last 5 rows of data:")
+st.dataframe(data.tail())
